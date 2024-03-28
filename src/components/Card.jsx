82e14f4ff1a12faab88720feb7770872e08731c1
Sanilal P Sathyan <sanilal@gmail.com>
2024-03-28 17:59:15 +0400

    card added

---
+++ src/components/Card.jsx
@@ -0,0 +1,35 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import {FiThumbsUp} from  "react-icons/fi";
+export default function Card({result}) {
+  return (
+    <div className='group'>
+        <div className=" bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+        <Link href={`/movie/${result.id}`}>
+            <Image src={`https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster_path}`}
+            width={500}
+            height={300}
+                    alt="Product" className="h-80 w-72 object-cover rounded-t-xl group-hover:opacity-60 transition-opacity duration-300" ></Image>
+
+            <div className="px-4 py-3 w-72">
+                <span className="text-gray-400 mr-3 uppercase text-xs">{result.original_language}</span>
+                <h2 className="text-lg font-bold text-black truncate block capitalize">{result.title || result.name}</h2>
+                <div className="flex flex-col items-start justify-items-center">
+                    <div className="flex flex-row justify-between">
+                    <p className="text-lg font-semibold text-black cursor-auto my-3">{result.vote_average}</p>
+                    <p className="text-sm text-gray-600 cursor-auto ml-2 my-3">{result.vote_count}</p>
+                    </div>
+                    <div className="ml-auto w-fit">
+                        <p className="text-gray-600 line-clamp-2 text-md">
+                            {result.overview}
+                        </p>
+                        <p>{result.release_date || result.first_air_date}</p>
+                        <FiThumbsUp />
+                    </div>
+                </div>
+            </div>
+        </Link>
+    </div>
+    </div>
+  )
+}
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,31 +1,12 @@
+import Card from "./Card";
+
 export default function Results({ results }) {
   return (
     <div id="products" className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
         {
             results.map((result)=>(
-                <div key={result.id}>
-                    <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-        <a href="#">
-            <img src="https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-                    alt="Product" className="h-80 w-72 object-cover rounded-t-xl" />
-            <div className="px-4 py-3 w-72">
-                <span className="text-gray-400 mr-3 uppercase text-xs">{result.original_language}</span>
-                <h2 className="text-lg font-bold text-black truncate block capitalize">{result.original_title}</h2>
-                <div className="flex flex-col items-start justify-items-center">
-                    <div className="flex flex-row justify-between">
-                    <p className="text-lg font-semibold text-black cursor-auto my-3">{result.vote_average}</p>
-                    <p className="text-sm text-gray-600 cursor-auto ml-2 my-3">{result.vote_count}</p>
-                    </div>
-                    <div className="ml-auto w-fit">
-                        <p className="text-gray-600">
-                            {result.overview}
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </a>
-    </div>
-                </div>
+                <Card key={result.id} result={result} />
+                
             ))
         }
     </div>
