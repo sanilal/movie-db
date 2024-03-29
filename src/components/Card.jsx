e6a9df1fab4ede1ad3e7dc76574b6e20eb36d336
Sanilal P Sathyan <sanilal@gmail.com>
2024-03-29 08:30:03 +0400

    Card updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,7 +11,7 @@ export default function Card({result}) {
             height={300}
                     alt="Product" className="h-80 w-72 object-cover rounded-t-xl group-hover:opacity-60 transition-opacity duration-300" ></Image>
 
-            <div className="px-4 py-3 w-72">
+            <div className="px-4 py-3 w-72 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 transition-shadow duration-200">
                 <span className="text-gray-400 mr-3 uppercase text-xs">{result.original_language}</span>
                 <h2 className="text-lg font-bold text-black truncate block capitalize">{result.title || result.name}</h2>
                 <div className="flex flex-col items-start justify-items-center">
@@ -23,8 +23,10 @@ export default function Card({result}) {
                         <p className="text-gray-600 line-clamp-2 text-md">
                             {result.overview}
                         </p>
-                        <p>{result.release_date || result.first_air_date}</p>
+                        <p>{result.release_date || result.first_air_date}
                         <FiThumbsUp />
+                        {result.vote_count}
+                        </p>
                     </div>
                 </div>
             </div>
